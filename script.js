eeba24a62e21fdbc6ab7aee797e35cee3b4f1eac
Daniel Richter <drichter3697@gmail.com>
2024-07-12 20:15:20 -0500

    small tweaks to design, added a search function (currently works with last name and id number) and some logic changes to encompass that

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let storage = []
+let table = document.querySelector(`#tableBody`)
 function addEmployee(event){
     event.preventDefault()
-    let table = document.querySelector(`#tableBody`)
     let fName = document.querySelector('#fName').value
     let lName = document.querySelector('#lName').value
     let idNum = document.querySelector('#idNum').value
@@ -13,25 +13,26 @@ function addEmployee(event){
             return
         }
     }
-    table.innerHTML += `
-    <tr>
-        <td>${fName}</td>
-        <td>${lName}</td>
-        <td>${idNum}</td>
-        <td>${jTitle}</td>
-        <td>${salary}</td>
-        <td><button onClick="deleteEmployee(event, ${idNum})">X</button>
-    </tr>
-    `
     storage.push({
-        name: fName + lName,
+        fName: fName,
+        lName: lName,
         idNum: idNum,
         jTitle: jTitle,
         salary: salary
     })
-    console.log(storage)
+    // table.innerHTML += `
+    // <tr>
+    //     <td>${fName}</td>
+    //     <td>${lName}</td>
+    //     <td>${idNum}</td>
+    //     <td>${jTitle}</td>
+    //     <td>${salary}</td>
+    //     <td><button onClick="deleteEmployee(event, ${idNum})">X</button>
+    // </tr>
+    // `
     calculateMonthly(salary)
     clearFields()
+    populateTable()
 }
 let monthlySalary = 0
 function calculateMonthly(num){
@@ -47,6 +48,7 @@ function calculateMonthly(num){
 }
 
 function deleteEmployee(event, inputNum){
+    if(confirm(`Are you sure you'd like to delete this entry?`))
     for(let i = 0; i < storage.length; i++){
         console.log(storage[i])
         if(Number(storage[i].idNum) === inputNum){
@@ -78,4 +80,45 @@ function clearFields(){
     idNum.value = ``
     jTitle.value = ``
     salary.value = ``
+}
+
+function populateTable(popEvent){
+    table.innerHTML = ``
+    let searchQuery = document.querySelector(`#search`).value
+    console.log(searchQuery)
+    if(popEvent === undefined){
+        for(let employee of storage){
+            table.innerHTML += `
+                <tr>
+                 <td>${employee.fName}</td>
+                 <td>${employee.lName}</td>
+                 <td>${employee.idNum}</td>
+                 <td>${employee.jTitle}</td>
+                 <td>${employee.salary}</td>
+                <td><button onClick="deleteEmployee(event, ${employee.idNum})">X</button>
+                </tr>
+                `
+        }
+    }else if(popEvent){
+        popEvent.preventDefault()
+        let returnArray = []
+        for(let employee of storage){
+            if(searchQuery === employee.idNum|| searchQuery === employee.lName)
+                returnArray.push(employee)
+        }
+        for(let match of returnArray){
+            table.innerHTML +=`
+            <tr>
+            <td>${match.fName}</td>
+            <td>${match.lName}</td>
+            <td>${match.idNum}</td>
+            <td>${match.jTitle}</td>
+            <td>${match.salary}</td>
+           <td><button onClick="deleteEmployee(event, ${match.idNum})">X</button>
+           </tr>
+           `
+        } document.querySelector(`#search`).value = ``
+    }else{
+        window.alert(`Error!`)
+    }
 }
